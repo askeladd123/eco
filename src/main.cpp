d787a881decc745062d39b6ef93ebceeedf7a3e4
Ask Sødal <asks@uia.no>
2022-05-30 20:03:50 +0200

    SFML linked

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+
+/*
+ * Ansvar for flyt i programmet
+ */
+
+#include <iostream>
+#include "globals.h"
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+  // create the window
+  sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 800)), "My window");
+  
+  // run the program as long as the window is open
+  while (window.isOpen())
+  {
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+    
+    // clear the window with black color
+    window.clear(sf::Color::Black);
+    
+    // draw everything here...
+    // window.draw(...);
+    
+    // end the current frame
+    window.display();
+  }
+  
+  return 0;
+}
