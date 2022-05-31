9c024acb1398ed0468f73ba7c5ccba05d197584b
Ask Sødal <asks@uia.no>
2022-05-31 12:20:18 +0200

    jobbet med collision funksjoner

--- src/collision.h
+++ src/collision.h
@@ -5,4 +5,46 @@
 #ifndef ECO_COLLISION_H
 #define ECO_COLLISION_H
 
+#include <cmath>
+
+struct point  {int x, y;};
+struct box    {int x, y, w, h;};
+struct circle {int x, y, r;};
+struct line   {int x1, y1, x2, y2;};
+
+struct intersection_r {bool intersects;};
+struct points_R {int points; point a, b;};
+
+bool collision(point a, point b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+
+bool collision(box a, box b)
+{
+  return
+  b.x < a.x + a.w &&
+  a.x < b.x + b.w &&
+  b.y < a.y + a.h &&
+  a.y < b.y + b.h;
+}
+
+bool collision(line a, line b);
+
+bool collision(circle a, circle b)
+{
+  int x = b.x - a.x;
+  int y = b.y - a.y;
+  return sqrt(x * x + y * y) < b.r - a.r;
+}
+
+bool collision(box b, circle c);
+bool collision(circle c, box b);
+
+bool collision(box b, line l);
+bool collision(line l, box b);
+
+bool collision(line l, circle c);
+bool collision(circle c, line l);
+
 #endif //ECO_COLLISION_H
--- src/world.cpp
+++ src/world.cpp
@@ -2,3 +2,10 @@
 // Ansvar for møte mellom skapninger og verden rundt
 //
 
+class world
+{
+  // list static objects;
+  // list moving objects;
+  
+  // TODO quad trees for collision
+};
